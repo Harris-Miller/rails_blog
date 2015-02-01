df10f506329c61686b2c6a56db6c40d639a5b572
Harris Miller <harrismiller08@gmail.com>
2015-02-01 00:11:48 -0700

    finished 'Getting Started' guide

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
   def index
   end
 
@@ -21,6 +24,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
